fc93cc9ddd8ffc7db239009296bb60d5fe913764
Isadora <flisadora@gmail.com>
2022-05-04 01:03:01 +0100

    grouping expenses

--- lib/screens/expenses_list.dart
+++ lib/screens/expenses_list.dart
@@ -4,9 +4,24 @@ import 'package:bytebank_persistence/database/dao/expense_dao.dart';
 import 'package:bytebank_persistence/models/expense.dart';
 import 'package:bytebank_persistence/screens/expense_form.dart';
 import 'package:flutter/material.dart';
+import 'package:grouped_list/grouped_list.dart';
 
 const _titleAppBar = 'Expenses';
 final ExpenseDao _dao = ExpenseDao();
+const Map<String,String> monthsInYear = {
+  '01': "January",
+  '02': "February",
+  '03': "March",
+  '04': "April",
+  '05': "May",
+  '06': "June",
+  '07': "July",
+  '08': "August",
+  '09': "September",
+  '10': "October",
+  '11': "November",
+  '12': "December"
+};
 
 class ExpensesList extends StatefulWidget {
   const ExpensesList({Key? key}) : super(key: key);
@@ -39,18 +54,32 @@ class ExpensesListState extends State<ExpensesList> {
                 final List<Expense> expenses = snapshot.data as List<Expense>;
                 if (expenses.isNotEmpty) {
                   expenses.sort((a, b) => b.date.compareTo(a.date));
-                  return ListView.builder(
-                    itemBuilder: (context, index) {
-                      final Expense expense = expenses[index];
+                  return GroupedListView<dynamic, String>(
+                    elements: expenses,
+                    groupBy: (expense) => expense.date.substring(5,7),
+                    groupComparator: (value1, value2) => value2.compareTo(value1),
+                    itemComparator: (item1, item2) =>
+                    item2.date.substring(8).compareTo(item1.date.substring(8)),
+                    order: GroupedListOrder.ASC,
+                    useStickyGroupSeparators: true,
+                    groupSeparatorBuilder: (String value) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        value,
+                        textAlign: TextAlign.left,
+                        style: TextStyle(fontSize: 20, fontWeight:
+                        FontWeight.bold),
+                      ),
+                    ),
+                    itemBuilder: (context, expense) {
                       return _ExpenseItem(expense, onClick: () {
                         Navigator.of(context).push(
-                            MaterialPageRoute(
-                                builder: (context) => ExpenseForm(expense)
-                            )
+                          MaterialPageRoute(
+                              builder: (context) => ExpenseForm(expense)
+                          )
                         );
                       },);
                     },
-                    itemCount: expenses.length,
                   );
                 }
               }
